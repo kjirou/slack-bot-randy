2b490edc7d59ea1d1395643f993a62331f7d21c8
kjirou <sorenariblog@gmail.com>
2019-01-16 16:18:36 +0900

    Make to get text from slack command

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 
-const app = express();
 const port = process.env.PORT || 5000;
 
+const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.post('/receive_slack_command', function (req, res) {
+  console.log(req.body);
+  res.send('Hello POST! = ' + req.body.text);
+});
+
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.send('Randy: https://github.com/kjirou/slack-bot-randy');
 });
 
 app.listen(port);
