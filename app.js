a7fa6ba3c09381dfef879f9cf995f3b8b6f12835
kjirou <sorenariblog@gmail.com>
2019-01-16 15:18:34 +0900

    Change default local port number to 5000
    The 5000 is used as default of `heroku local web`.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
   res.send('Hello World');
